fd9095f438de9b61d8944d333ca769f08f61892f
ildar <62477871+elli56@users.noreply.github.com>
2020-05-13 21:19:19 +0300

    Add files via upload

---
+++ Kappe-portfolio-test-3/js/control.js
@@ -0,0 +1,17 @@
+
+$(function(){
+    
+    "use strict";
+
+    $('#info').click( function() {                                   $('#info_box').toggleClass('open'); 
+        } );
+    
+    $('.pr_bar').each(function(){
+       let width = $(this).data('skill') //в переменную записали необходимую ширину 
+    $(this).css('width', width + '%');
+    });
+    
+     $('#menu_btn').click( function() {                                   $('#main_nav').toggleClass('open'); 
+        } );
+    
+    });
